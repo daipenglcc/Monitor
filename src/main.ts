e5cad89cfd8fdd3e4a9335fd8c0490d6f86bac80
daipeng <youhuabujianye@gmail.com>
2022-07-18 14:18:52 +0800

    feat: config

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,24 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import * as Sentry from '@sentry/vue'
+import { BrowserTracing } from '@sentry/tracing'
 
-import * as fundebug from 'fundebug-javascript'
-import FundebugVue from 'fundebug-vue'
-fundebug.apikey = '7032ad11af3d1cf7b454841998d7a2198ce96ae41ca4f36c31b89ab65afcd0a1'
+const app = createApp(App)
 
-createApp(App).use(new FundebugVue(fundebug)).mount('#app')
+Sentry.init({
+	app,
+	dsn: 'https://daf508a612a448edbf1688904c0f5d4d@o1323194.ingest.sentry.io/6580710',
+	integrations: [
+		new BrowserTracing({
+			// routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+			tracingOrigins: ['localhost', 'my-site-url.com', /^\//]
+		})
+	],
+	// Set tracesSampleRate to 1.0 to capture 100%
+	// of transactions for performance monitoring.
+	// We recommend adjusting this value in production
+	tracesSampleRate: 1.0
+})
+
+app.mount('#app')
